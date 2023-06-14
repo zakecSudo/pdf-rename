45e5c25d8ba8ee8b168d95cf5d03c035f060dc73
Zak Albreht <62850706+zakecSudo@users.noreply.github.com>
2023-06-14 17:41:40 +0200

    Update script.cmd

--- scripts/script.cmd
+++ scripts/script.cmd
@@ -7,28 +7,32 @@ set "source_excel_file=demo.xlsx"
 echo ----------------------------------------------------------------
 echo KOPIRAJ in PREIMENUJ %source_excel_file% datoteko iz .pdf datotek
 echo ----------------------------------------------------------------
-set /p "choice=Ali naj se skripta požene v trenutnem direktoriju? (y/n): "
+set /p "choice=Ali naj se skripta pozene v trenutnem direktoriju? (y/n): "
 
 if /i "%choice%"=="n" (
-    set /p "folder=Vnesi pot kjer naj se skripta požene: "
+    set /p "folder=Vnesi pot kjer naj se skripta pozene: "
+    echo $folder
     cd "%folder%"
+    echo TEST
+    echo $PWD
+    echo TEST
 )
 
 :: Kopiraj demo.xlsx za vsak .pdf file
 set "new_files=0"
+setlocal enabledelayedexpansion
+
 for /r %%F in (*.pdf) do (
     set "pdf_file=%%F"
-    setlocal enabledelayedexpansion
     set "excel_file=!pdf_file:.pdf=.xlsx!"
 
     :: Preveri če tak file ne obstaja
     if not exist "!excel_file!" (
-        copy "%source_excel_file%" "!excel_file!"
-        set /a "new_files+=1"
+        copy "%source_excel_file%" "!excel_file!" >NUL
+        set /A "new_files+=1"
     )
-    echo !new_files!
-    endlocal
 )
 
 echo.
-echo Uspešno kreiranih %new_files:~-1% novih datotek.
+echo Uspesno kreiranih !new_files! novih datotek.
+pause
